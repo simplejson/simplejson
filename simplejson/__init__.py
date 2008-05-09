74d9c5c4c4339db47dfa86bf37858cae80ed3776
Bob Ippolito <bob@redivi.com>
2008-05-09 16:41:57 +0000

    use -msimplejson.tool instead of -msimplejson to match python 2.6 json library
    git-svn-id: http://simplejson.googlecode.com/svn/trunk@95 a4795897-2c25-0410-b006-0d3caba88fa1

--- simplejson/__init__.py
+++ simplejson/__init__.py
@@ -89,23 +89,25 @@ Extending JSONEncoder::
 Using simplejson from the shell to validate and
 pretty-print::
     
-    $ echo '{"json":"obj"}' | python -msimplejson
+    $ echo '{"json":"obj"}' | python -msimplejson.tool
     {
         "json": "obj"
     }
-    $ echo '{ 1.2:3.4}' | python -msimplejson
+    $ echo '{ 1.2:3.4}' | python -msimplejson.tool
     Expecting property name: line 1 column 2 (char 2)
 
 Note that the JSON produced by this module's default settings
 is a subset of YAML, so it may be used as a serializer for that as well.
 """
-__version__ = '1.9.1'
+__version__ = '1.9.2'
 __all__ = [
     'dump', 'dumps', 'load', 'loads',
     'JSONDecoder', 'JSONEncoder',
 ]
 
 if __name__ == '__main__':
+    import warnings
+    warnings.warn('python -msimplejson is deprecated, use python -msiplejson.tool', DeprecationWarning)
     from simplejson.decoder import JSONDecoder
     from simplejson.encoder import JSONEncoder
 else:
@@ -369,32 +371,6 @@ def write(obj):
     return dumps(obj)
 
 
-#
-# Pretty printer:
-#     curl http://mochikit.com/examples/ajax_tables/domains.json | python -msimplejson
-#
-
-
-def main():
-    import sys
-    if len(sys.argv) == 1:
-        infile = sys.stdin
-        outfile = sys.stdout
-    elif len(sys.argv) == 2:
-        infile = open(sys.argv[1], 'rb')
-        outfile = sys.stdout
-    elif len(sys.argv) == 3:
-        infile = open(sys.argv[1], 'rb')
-        outfile = open(sys.argv[2], 'wb')
-    else:
-        raise SystemExit("%s [infile [outfile]]" % (sys.argv[0],))
-    try:
-        obj = load(infile)
-    except ValueError, e:
-        raise SystemExit(e)
-    dump(obj, outfile, sort_keys=True, indent=4)
-    outfile.write('\n')
-
-
 if __name__ == '__main__':
-    main()
+    import simplejson.tool
+    simplejson.tool.main()
