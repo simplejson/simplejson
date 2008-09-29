86137c849d5e8f5e11fbe4ca180835bdc592f894
Bob Ippolito <bob@redivi.com>
2008-09-29 19:55:17 +0000

    failing test for long, thanks @djco
    git-svn-id: http://simplejson.googlecode.com/svn/trunk@133 a4795897-2c25-0410-b006-0d3caba88fa1

--- simplejson/tests/test_float.py
+++ simplejson/tests/test_float.py
@@ -7,3 +7,8 @@ class TestFloat(TestCase):
     def test_floats(self):
         for num in [1617161771.7650001, math.pi, math.pi**100, math.pi**-100]:
             self.assertEquals(float(S.dumps(num)), num)
+
+    def test_ints(self):
+        for num in [1, 1L, 1<<32, 1<<64]:
+            self.assertEquals(S.dumps(num), str(num))
+            self.assertEquals(int(S.dumps(num)), num)
