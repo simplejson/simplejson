be8ae2ce64b8a646bf973d87ddf37269e35cc85f
Bob Ippolito <bob@redivi.com>
2008-10-24 16:30:09 +0000

    http://code.google.com/p/simplejson/issues/detail?id=27
    git-svn-id: http://simplejson.googlecode.com/svn/trunk@148 a4795897-2c25-0410-b006-0d3caba88fa1

--- simplejson/tests/test_float.py
+++ simplejson/tests/test_float.py
@@ -5,8 +5,9 @@ import simplejson as S
 
 class TestFloat(TestCase):
     def test_floats(self):
-        for num in [1617161771.7650001, math.pi, math.pi**100, math.pi**-100]:
+        for num in [1617161771.7650001, math.pi, math.pi**100, math.pi**-100, 3.1]:
             self.assertEquals(float(S.dumps(num)), num)
+            self.assertEquals(S.loads(S.dumps(num)), num)
 
     def test_ints(self):
         for num in [1, 1L, 1<<32, 1<<64]:
