85e1e6878525c63441d120c39a198f33cc392816
Bob Ippolito <bob@redivi.com>
2009-02-28 10:00:59 +0000

    comment about weird struct usage per http://codereview.appspot.com/20095/diff/1/13#newcode21
    git-svn-id: http://simplejson.googlecode.com/svn/trunk@175 a4795897-2c25-0410-b006-0d3caba88fa1

--- simplejson/decoder.py
+++ simplejson/decoder.py
@@ -16,6 +16,8 @@ FLAGS = re.VERBOSE | re.MULTILINE | re.DOTALL
 
 def _floatconstants():
     _BYTES = '7FF80000000000007FF0000000000000'.decode('hex')
+    # The struct module in Python 2.4 would get frexp() out of range here
+    # when an endian is specified in the format string. Fixed in Python 2.5+
     if sys.byteorder != 'big':
         _BYTES = _BYTES[:8][::-1] + _BYTES[8:][::-1]
     nan, inf = struct.unpack('dd', _BYTES)
