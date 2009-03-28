b943b2bb7fcecfefda78a24374b747d8590c8666
Bob Ippolito <bob@redivi.com>
2009-03-28 05:18:13 +0000

    version bump
    git-svn-id: http://simplejson.googlecode.com/svn/trunk@176 a4795897-2c25-0410-b006-0d3caba88fa1

--- simplejson/__init__.py
+++ simplejson/__init__.py
@@ -97,7 +97,7 @@ Using simplejson.tool from the shell to validate and pretty-print::
     $ echo '{ 1.2:3.4}' | python -m simplejson.tool
     Expecting property name: line 1 column 2 (char 2)
 """
-__version__ = '2.0.9'
+__version__ = '2.1.0'
 __all__ = [
     'dump', 'dumps', 'load', 'loads',
     'JSONDecoder', 'JSONEncoder',
