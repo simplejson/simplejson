c3ec0ef0842cf279cdc39e9fb9f353db63017d96
Bob Ippolito <bob@redivi.com>
2011-02-06 14:55:31 +0800

    remove docs from master

--- scripts/make_docs.py
+++ scripts/make_docs.py
@@ -11,6 +11,7 @@ for dirname in DOCTREES_DIR, HTML_DIR:
     if not os.path.exists(dirname):
         os.makedirs(dirname)
 
+open(os.path.join(HTML_DIR, '.nojekyll'), 'w').close()
 res = subprocess.call([
     SPHINX_BUILD, '-d', DOCTREES_DIR, '-b', 'html', '.', 'docs',
 ])
