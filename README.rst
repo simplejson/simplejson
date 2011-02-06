131842a8af5a513d8020e70f8f7c571502c3f778
Bob Ippolito <bob@redivi.com>
2011-02-06 14:13:21 +0800

    move long_description to README.rst

---
+++ README.rst
@@ -0,0 +1,15 @@
+simplejson is a simple, fast, complete, correct and extensible
+JSON <http://json.org> encoder and decoder for Python 2.5+.  It is
+pure Python code with no dependencies, but includes an optional C
+extension for a serious speed boost.
+
+simplejson is the externally maintained development version of the
+json library included with Python 2.6 and Python 3.0, but maintains
+backwards compatibility with Python 2.5.
+
+The encoder may be subclassed to provide serialization in any kind of
+situation, without any special support by the objects to be serialized
+(somewhat like pickle).
+
+The decoder can handle incoming JSON strings of any specified encoding
+(UTF-8 by default).
--- setup.py
+++ setup.py
@@ -14,23 +14,7 @@ from distutils.errors import CCompilerError, DistutilsExecError, \
 
 VERSION = '2.1.4'
 DESCRIPTION = "Simple, fast, extensible JSON encoder/decoder for Python"
-LONG_DESCRIPTION = """
-simplejson is a simple, fast, complete, correct and extensible
-JSON <http://json.org> encoder and decoder for Python 2.5+.  It is
-pure Python code with no dependencies, but includes an optional C
-extension for a serious speed boost.
-
-simplejson is the externally maintained development version of the
-json library included with Python 2.6 and Python 3.0, but maintains
-backwards compatibility with Python 2.5.
-
-The encoder may be subclassed to provide serialization in any kind of
-situation, without any special support by the objects to be serialized
-(somewhat like pickle).
-
-The decoder can handle incoming JSON strings of any specified encoding
-(UTF-8 by default).
-"""
+LONG_DESCRIPTION = open('README.rst', 'r').read()
 
 CLASSIFIERS = filter(None, map(str.strip,
 """
