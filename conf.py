eff8dd214e522d4eb6f56c8868ba92eb631dc914
Bob Ippolito <bob@redivi.com>
2011-04-19 13:01:07 -0700

    bump versions to 2.1.6

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ copyright = '2011, Bob Ippolito'
 # The short X.Y version.
 version = '2.1'
 # The full version, including alpha/beta/rc tags.
-release = '2.1.5'
+release = '2.1.6'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from distutils.errors import CCompilerError, DistutilsExecError, \
     DistutilsPlatformError
 
 IS_PYPY = hasattr(sys, 'pypy_translation_info')
-VERSION = '2.1.5'
+VERSION = '2.1.6'
 DESCRIPTION = "Simple, fast, extensible JSON encoder/decoder for Python"
 LONG_DESCRIPTION = open('README.rst', 'r').read()
 
