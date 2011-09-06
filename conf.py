52d00503192973f36e629c8869652df1b65eb8be
Bob Ippolito <bob@redivi.com>
2011-09-06 12:23:54 -0700

    fix https://github.com/simplejson/simplejson/issues/16 - include MANIFEST.in in MANIFEST.in. distutils is dumb.

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ copyright = '2011, Bob Ippolito'
 # The short X.Y version.
 version = '2.2'
 # The full version, including alpha/beta/rc tags.
-release = '2.2.0'
+release = '2.2.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
