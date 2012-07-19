f55e882aafa73732d63129cf1280c093acc916db
Janne Kulmala <janne.t.kulmala@tut.fi>
2012-07-19 12:59:49 +0300

    raw_decode(): Document 'idx' parameter

--- simplejson/decoder.py
+++ simplejson/decoder.py
@@ -413,6 +413,8 @@ class JSONDecoder(object):
         """Decode a JSON document from ``s`` (a ``str`` or ``unicode``
         beginning with a JSON document) and return a 2-tuple of the Python
         representation and the index in ``s`` where the document ended.
+        Optionally, ``idx`` can be used to specify an offset in ``s`` where
+        the JSON document begins.
 
         This can be used to decode a JSON document from a string that may
         have extraneous data at the end.
