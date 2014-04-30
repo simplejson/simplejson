88ac2f35519afb8099dbb7861ff476896d577206
Bob Ippolito <bob@redivi.com>
2014-04-30 11:43:21 -0700

    fix tests to run on Python 3.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from distutils.errors import CCompilerError, DistutilsExecError, \
     DistutilsPlatformError
 
 IS_PYPY = hasattr(sys, 'pypy_translation_info')
-VERSION = '3.4.0'
+VERSION = '3.4.1'
 DESCRIPTION = "Simple, fast, extensible JSON encoder/decoder for Python"
 
 with open('README.rst', 'r') as f:
