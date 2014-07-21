46d42a5245607745eb39da83f0ca250d9c3eff52
Bob Ippolito <bob@redivi.com>
2014-07-21 17:23:59 +0200

    v3.6.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from distutils.errors import CCompilerError, DistutilsExecError, \
     DistutilsPlatformError
 
 IS_PYPY = hasattr(sys, 'pypy_translation_info')
-VERSION = '3.5.3'
+VERSION = '3.6.0'
 DESCRIPTION = "Simple, fast, extensible JSON encoder/decoder for Python"
 
 with open('README.rst', 'r') as f:
