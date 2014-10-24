d52cd59d28766c72314537e2a087665b8b54fa9b
Bob Ippolito <bob@redivi.com>
2014-10-24 08:14:19 -0700

    update CHANGES and bump version to v3.6.5

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ copyright = '2014, Bob Ippolito'
 # The short X.Y version.
 version = '3.6'
 # The full version, including alpha/beta/rc tags.
-release = '3.6.4'
+release = '3.6.5'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
