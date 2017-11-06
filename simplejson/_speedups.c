c9733f38bd45e2d825a4ca85d178ebca94877937
Serhiy Storchaka <storchaka@gmail.com>
2017-11-06 21:28:31 +0200

    bpo-31095: fix potential crash during GC Original patch by INADA Naoki.

--- simplejson/_speedups.c
+++ simplejson/_speedups.c
@@ -1355,7 +1355,8 @@ py_encode_basestring_ascii(PyObject* self UNUSED, PyObject *pystr)
 static void
 scanner_dealloc(PyObject *self)
 {
-    /* Deallocate scanner object */
+    /* bpo-31095: UnTrack is needed before calling any callbacks */
+    PyObject_GC_UnTrack(self);
     scanner_clear(self);
     Py_TYPE(self)->tp_free(self);
 }
@@ -3205,7 +3206,8 @@ bail:
 static void
 encoder_dealloc(PyObject *self)
 {
-    /* Deallocate Encoder */
+    /* bpo-31095: UnTrack is needed before calling any callbacks */
+    PyObject_GC_UnTrack(self);
     encoder_clear(self);
     Py_TYPE(self)->tp_free(self);
 }
