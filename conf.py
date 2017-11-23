61fae0c2dac6a0769a0b6e49320e613e498adc11
Bob Ippolito <bob@redivi.com>
2017-11-23 12:37:30 -0800

    v3.12.1

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ copyright = '2017, Bob Ippolito'
 # The short X.Y version.
 version = '3.12'
 # The full version, including alpha/beta/rc tags.
-release = '3.12.0'
+release = '3.12.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
