3cf08530a1be05cbaa80b167fe7f335b13cbc869
Bob Ippolito <bob@redivi.com>
2017-11-24 09:44:12 -0800

    v3.13.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from distutils.errors import CCompilerError, DistutilsExecError, \
     DistutilsPlatformError
 
 IS_PYPY = hasattr(sys, 'pypy_translation_info')
-VERSION = '3.13.1'
+VERSION = '3.13.2'
 DESCRIPTION = "Simple, fast, extensible JSON encoder/decoder for Python"
 
 with open('README.rst', 'r') as f:
