simplejson/simplejson

commit d78256146d2831e3a7080b7212d7edb90bc2d611

Fixes #144. Workaround for bad behavior in string subclasses.

simplejson/encoder.py
@@ -5,7 +5,7 @@ import re
 from operator import itemgetter
 # Do not import Decimal directly to avoid reload issues
 import decimal
-from .compat import u, unichr, binary_type, string_types, integer_types, PY3
+from .compat import u, unichr, binary_type, text_type, string_types, integer_types, PY3
 def _import_speedups():
     try:
         from . import _speedups
@@ -46,9 +46,13 @@ def encode_basestring(s, _PY3=PY3, _q=u('"')):
     if _PY3:
         if isinstance(s, binary_type):
             s = s.decode('utf-8')
+        if type(s) is not text_type:
+            s = text_type(s)
     else:
         if isinstance(s, str) and HAS_UTF8.search(s) is not None:
             s = s.decode('utf-8')
+        if type(s) not in string_types:
+            s = text_type(s)
     def replace(match):
         return ESCAPE_DCT[match.group(0)]
     return _q + ESCAPE.sub(replace, s) + _q
@@ -61,9 +65,13 @@ def py_encode_basestring_ascii(s, _PY3=PY3):
     if _PY3:
         if isinstance(s, binary_type):
             s = s.decode('utf-8')
+        if type(s) is not text_type:
+            s = text_type(s)
     else:
         if isinstance(s, str) and HAS_UTF8.search(s) is not None:
             s = s.decode('utf-8')
+        if type(s) not in string_types:
+            s = text_type(s)
     def replace(match):
         s = match.group(0)
         try:

simplejson/tests/test_str_subclass.py
@@ -1,0 +1,16 @@
+from unittest import TestCase
+
+import simplejson
+from simplejson.compat import text_type, u
+
+# Tests for issue demonstrated in https://github.com/simplejson/simplejson/issues/144
+class WonkyTextSubclass(text_type):
+    def __getslice__(self, start, end):
+        return self.__class__('not what you wanted!')
+
+class TestStrSubclass(TestCase):
+    def test_dump_load(self):
+        for s in ['', '"hello"', 'text', u('\u005c')]:
+            self.assertEqual(
+                s,
+                simplejson.loads(simplejson.dumps(WonkyTextSubclass(s))))

simplejson/tests/__init__.py
@@ -57,6 +57,7 @@ def all_tests_suite():
                 'simplejson.tests.test_scanstring',
                 'simplejson.tests.test_separators',
                 'simplejson.tests.test_speedups',
+                'simplejson.tests.test_str_subclass',
                 'simplejson.tests.test_unicode',
                 'simplejson.tests.test_decimal',
                 'simplejson.tests.test_tuple',

commit 4e4ee05652472acb3bde31c5ad138b35d6c7dce9

Avoid copying strings when encoding
d782561 introduced a performance regression which means that every string is
copied during serialisation, when ensure_ascii is False.

This should fix it.

simplejson/encoder.py
@@ -52,7 +52,7 @@ def encode_basestring(s, _PY3=PY3, _q=u('"')):
     else:
         if isinstance(s, str) and HAS_UTF8.search(s) is not None:
             s = s.decode('utf-8')
-        if type(s) not in string_types:
+        if type(s) not in (str, unicode):
             if isinstance(s, str):
                 s = str.__str__(s)
             else:
@@ -74,7 +74,7 @@ def py_encode_basestring_ascii(s, _PY3=PY3):
     else:
         if isinstance(s, str) and HAS_UTF8.search(s) is not None:
             s = s.decode('utf-8')
-        if type(s) not in string_types:
+        if type(s) not in (str, unicode):
             if isinstance(s, str):
                 s = str.__str__(s)
             else:

simplejson/tests/test_str_subclass.py
@@ -14,3 +14,8 @@ class TestStrSubclass(TestCase):
             self.assertEqual(
                 s,
                 simplejson.loads(simplejson.dumps(WonkyTextSubclass(s))))
+
+            self.assertEqual(
+                s,
+                simplejson.loads(simplejson.dumps(WonkyTextSubclass(s),
+                                                  ensure_ascii=False)))