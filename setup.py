850fb8d1e7733879018ad8618d032e535fa47fe4
Bob Ippolito <bob@redivi.com>
2021-08-19 10:58:29 -0700

    Upgrade cibuildwheel to v2.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from distutils.errors import CCompilerError, DistutilsExecError, \
     DistutilsPlatformError
 
 IS_PYPY = hasattr(sys, 'pypy_translation_info')
-VERSION = '3.17.3'
+VERSION = '3.17.4'
 DESCRIPTION = "Simple, fast, extensible JSON encoder/decoder for Python"
 
 with open('README.rst', 'r') as f:
