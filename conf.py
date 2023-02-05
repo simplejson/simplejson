08991e091d314e0a200711e0b7b401fb6970d868
Bob Ippolito <bob@redivi.com>
2023-02-05 09:31:07 -0800

    Fix #309. Build sdist without any overrides

--- conf.py
+++ conf.py
@@ -36,7 +36,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'simplejson'
-copyright = '2022, Bob Ippolito'
+copyright = '2023, Bob Ippolito'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
@@ -44,7 +44,7 @@ copyright = '2022, Bob Ippolito'
 # The short X.Y version.
 version = '3.18'
 # The full version, including alpha/beta/rc tags.
-release = '3.18.2'
+release = '3.18.3'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
