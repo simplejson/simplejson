f6dfa66b5f8494eca4d2773f9ba54bdbbbbfaa20
Bob Ippolito <bob@redivi.com>
2025-02-14 10:55:22 -0800

    Fix #328, memoize keys after ensuring that they are strings

--- simplejson/tests/test_dump.py
+++ simplejson/tests/test_dump.py
@@ -73,6 +73,11 @@ class TestDump(TestCase):
                  {True: False, False: True}, sort_keys=True),
                  '{"false": true, "true": false}')
         self.assertEqual(
+            json.dumps({'k1': {False: 5}, 'k2': {0: 5}}),
+            '{"k1": {"false": 5}, "k2": {"0": 5}}'
+        )
+
+        self.assertEqual(
             json.dumps(
                 {2: 3.0,
                  4.0: long_type(5),
