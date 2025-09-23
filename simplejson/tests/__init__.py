feb07a3f18a0c5672af154fbc8bb4e39a638242d
Tim Felgentreff <tim.felgentreff@oracle.com>
2025-09-23 19:44:19 +0200

    Test pure Python code, also on PyPy and GraalPy

--- simplejson/tests/__init__.py
+++ simplejson/tests/__init__.py
@@ -18,7 +18,7 @@ class TestMissingSpeedups(unittest.TestCase):
     def runTest(self):
         if hasattr(sys, "pypy_translation_info"):
             "PyPy doesn't need speedups! :)"
-        elif getattr(getattr(sys, "implementation", None), "name", None) == "graalpy"
+        elif getattr(getattr(sys, "implementation", None), "name", None) == "graalpy":
             "GraalPy doesn't need speedups! :)"
         elif hasattr(self, "skipTest"):
             self.skipTest("_speedups.so is missing!")
